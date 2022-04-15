a01a228431529b66406ec91cd24f186b47f81cc1
hernandojr <hernandocostelo@gmail.com>
2022-04-15 23:10:59 +0800

    Updated files

--- UserPATH.py
+++ UserPATH.py
@@ -3,13 +3,13 @@ import cv2
 
 
 while True:
-    
-    print("AVAILABLE PICTURES IN MY FOLDER: \n Just type \n >heart.png \n >star.png")
+    print("=================================================================")
+    print("AVAILABLE PICTURES IN MY FOLDER(Type the filename with extension!):\n>heart.png \n>star.png")
     image_filename = input("\n Input the filename >>> ")
-    print("Image effects list just type the following:  \n >colored \n >grayscale")
-    image_flag = input("Image effects >>>")
+    print("Image effects:\n","Press[0] Grayscale\n Press[1] Colored")
+    image_flag = input("Image effects flag: >>>")
 
-    if os.path.exists(image_filename) and image_flag == 'colored':
+    if os.path.exists(image_filename) and image_flag == '1':
         
         img = cv2.imread(image_filename, 1)
         cv2.imshow(image_filename,img)
@@ -18,13 +18,14 @@ while True:
         print("done")
         break
         
-    elif os.path.exists(image_filename) and image_flag == 'grayscale':
+    elif os.path.exists(image_filename) and image_flag == '0':
          
-        img = c.imread(image_filename,0)
+        img = cv2.imread(image_filename,0)
         cv2.imshow(image_filename, img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         print("Done!")
         break
     else:
-        print("File not found.")
+        
+        print("\n--File not found or Invalid Input--")
